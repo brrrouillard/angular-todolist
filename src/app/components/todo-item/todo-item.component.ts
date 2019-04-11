768e5c942a7b98d3a4d05050ad7f66d8fa5278f2
Matthieu Brouillard <mbrouilla@gmail.com>
2019-04-12 00:03:07 +0200

    Add some todo-item styling

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -7,11 +7,20 @@ import { Todo } from 'src/app/models/Todo';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
-  
+
   @Input() todo: Todo;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // Set dynamic classes
+  setClasses(){
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+    return classes;
   }
 
 }
