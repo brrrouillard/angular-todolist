3c9bd39c0720215fa9eae6a8cd18146831efae28
Matthieu Brouillard <mbrouilla@gmail.com>
2019-04-12 16:14:10 +0200

    Add API PUT request service

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,13 +10,16 @@ import { Todo } from 'src/app/models/Todo';
 export class TodoItemComponent implements OnInit {
 
   @Input() todo: Todo;
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
   }
 
   onToggle(todo){
+    // Toggle in UI
     todo.completed = !todo.completed;
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
   }
 
   // Set dynamic classes
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,19 +1,32 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-import { Todo } from "../models/Todo";
+import { Todo } from "../models/Todo"; 
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TodoService {
-  todosUrl:string = 'http://jsonplaceholder.typicode.com/todos';
-  todosLimit:string = "?_limit=5"
+  todosUrl: string = "http://jsonplaceholder.typicode.com/todos";
+  todosLimit: string = "?_limit=5";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getTodos():Observable<Todo[]> {
+  toggleCompleted(todo: Todo):Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
+}
+
+  getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(this.todosUrl + this.todosLimit);
   }
+
+  
 }
